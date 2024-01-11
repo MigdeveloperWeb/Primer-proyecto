bc80bb8e428f0779f2f0e84ad46441f50975a106
Migdevpro <134132545+Migdevpro@users.noreply.github.com>
2024-01-11 05:54:50 -0400

    Comprobando la conexion de la base de datos

--- includes/database.php
+++ includes/database.php
@@ -1,3 +1,9 @@
 <?php
-$db = mysqli_connect('localhost', 'root', 'Mavspro95', 'appsalon')
+$db = mysqli_connect('localhost', 'root', 'Mavspro95', 'appsalon');
+
+if(!$db){
+    echo "Hubo un error";
+}else{
+    echo "Conexion establecida correctamente";
+}
 ?>
