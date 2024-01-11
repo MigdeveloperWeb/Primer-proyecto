086be4f4587288517f3e26eae10edcda3de56b93
Migdevpro <134132545+Migdevpro@users.noreply.github.com>
2024-01-11 06:13:01 -0400

    Comprobando si la base de datos esta conectada correctamente

---
+++ includes/funciones.php
@@ -0,0 +1,18 @@
+<?php
+
+function obtenerServicios(){
+    try{
+        require 'database.php';
+
+        $sql = "SELECT * FROM servicios;";
+
+        $consulta = mysqli_query($db, $sql);
+
+        echo "<pre>";
+        var_dump(mysqli_fetch_assoc($consulta));
+        echo "</pre>";
+
+    }catch(\Throwable $th) {
+
+    }
+}
